71bf7a25097379e9fd4731111604a98ae46a1f16
Justin Kenyon <kenyonj@gmail.com>
2014-04-14 17:38:21 -0400

    Add null protection in discussions table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414210048) do
+ActiveRecord::Schema.define(version: 20140414213454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20140414210048) do
   create_table "discussions", force: true do |t|
     t.integer  "user_id",      null: false
     t.string   "name",         null: false
-    t.integer  "content_id"
-    t.string   "content_type"
+    t.integer  "content_id",   null: false
+    t.string   "content_type", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
