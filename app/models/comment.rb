5fe0fba81d7d5cb0b65236865c6303536f173613
Justin Kenyon <kenyonj@gmail.com>
2014-04-16 11:42:24 -0400

    remove redundant curly braces

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,10 @@ class Comment < ActiveRecord::Base
   after_save :push_update
 
   def push_update
-    Pusher['test_channel'].trigger('my_event', {
+    Pusher['test_channel'].trigger('my_event',
       username: username,
       body: body,
       created_at: created_at
-    })
+    )
   end
 end
