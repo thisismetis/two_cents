59042e289e00d7ce0a4b86dee2d846332e34959f
John Bagley <jdbagley@gmail.com>
2014-04-16 15:07:43 -0400

    ensure randomness in token creation

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -5,21 +5,21 @@ class Discussion < ActiveRecord::Base
 
   validates :name, presence: true
   validates :user, presence: true
-  validates :token, presence: true, uniqueness: true
 
   has_attached_file :photo, styles: { medium: '700x700>' }
   validates_attachment_content_type :photo, content_type: /\Aimage/
   validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g\Z/]
 
   def to_param
-    self.token
+    token
   end
 
   private
 
   def generate_token
     self.token = SecureRandom.hex(8)
+    while Discussion.exists?(token: token)
+      self.token = SecureRandom.hex(8)
+    end
   end
 end
-
-
