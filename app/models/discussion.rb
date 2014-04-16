9b783b782cfa45d8e32ad0eafc0ee8b8e5aef4ea
John Bagley <jdbagley@gmail.com>
2014-04-16 14:19:35 -0400

    saving so i can pull from master

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,6 @@
 class Discussion < ActiveRecord::Base
   belongs_to :user
-
+  before_create :generate_token
   has_many :comments
 
   validates :name, presence: true
@@ -9,4 +9,16 @@ class Discussion < ActiveRecord::Base
   has_attached_file :photo, styles: { medium: '700x700>' }
   validates_attachment_content_type :photo, content_type: /\Aimage/
   validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g\Z/]
+
+  def initialize
+    @token = token || generate_token
+  end
+
+  private
+
+  def generate_token
+    @token = SecureRandom.base64(8)
+  end
 end
+
+
