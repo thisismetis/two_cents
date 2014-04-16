e6dc9fd414c960896b76349c11c229ff1fcdf03a
Justin Kenyon <kenyonj@gmail.com>
2014-04-16 13:43:56 -0400

    update ENV.fetch calls

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,4 +1,4 @@
 require 'pusher'
 
-Pusher.url = "http://#{ENV.fetch['PUSHER_KEY']}:#{ENV.fetch['PUSHER_SECRET']}@api.pusherapp.com/apps/#{ENV.fetch['PUSHER_APP_ID']}"
+Pusher.url = "http://#{ENV.fetch('PUSHER_KEY')}:#{ENV.fetch('PUSHER_SECRET')}@api.pusherapp.com/apps/#{ENV.fetch('PUSHER_APP_ID')}"
 Pusher.logger = Rails.logger
