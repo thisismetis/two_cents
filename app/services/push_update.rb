138d92e5f989a2e3404feaadb94a08280350bef2
Justin Kenyon <kenyonj@gmail.com>
2014-04-16 13:52:55 -0400

    rename test_channel to comments_updater and my_event to comment_event

--- app/services/push_update.rb
+++ app/services/push_update.rb
@@ -5,7 +5,7 @@ class PushUpdate
 
   def push_update
     if subject.valid?
-      Pusher['test_channel'].trigger('my_event',
+      Pusher['comments_updater'].trigger('comment_event',
         username: subject.username,
         body: subject.body,
         created_at: subject.created_at
