fda54d8548565c16f42c9880c25fada8b3ee08a3
Justin Kenyon <kenyonj@gmail.com>
2014-04-17 16:06:08 -0400

    Add js for dynamic content form

---
+++ app/assets/javascripts/content_form.js
@@ -0,0 +1,21 @@
+function changeType(type) {
+  $('#content_discussion_form_type').attr('value', type);
+  if(type === 'Code' || type === 'Video')
+    {
+      $('#content-form-subject').html('<input id="content_discussion_form_subject" name="content_discussion_form[subject]" type="text">');
+    }
+  else
+    {
+      $('#content-form-subject').html('<input id="content_discussion_form_subject" name="content_discussion_form[subject]" type="file">');
+    }
+
+  if(type === 'Video')
+    {
+      $('#content_discussion_form_subject').attr('placeholder', 'Enter Youtube URL');
+    }
+
+  if(type === 'Code')
+    {
+      $('#content_discussion_form_subject').attr('placeholder', 'Enter Gist URL');
+    }
+};
