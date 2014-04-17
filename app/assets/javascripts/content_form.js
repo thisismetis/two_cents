a822d731fb803000cb974559ac66218018c24e2b
Justin Kenyon <kenyonj@gmail.com>
2014-04-17 16:28:52 -0400

    change js onclick to jquery .click()

--- app/assets/javascripts/content_form.js
+++ app/assets/javascripts/content_form.js
@@ -1,21 +1,21 @@
-function changeType(type) {
-  $('#content_discussion_form_type').attr('value', type);
-  if(type === 'Code' || type === 'Video')
-    {
+$(document).ready(function() {
+  $('.change-type').click(function() {
+    var type = $(this).data('type');
+
+    $('#content_discussion_form_type').attr('value', type);
+
+    if(type === 'Code' || type === 'Video') {
       $('#content-form-subject').html('<input id="content_discussion_form_subject" name="content_discussion_form[subject]" type="text">');
-    }
-  else
-    {
+    } else {
       $('#content-form-subject').html('<input id="content_discussion_form_subject" name="content_discussion_form[subject]" type="file">');
     }
 
-  if(type === 'Video')
-    {
+    if(type === 'Video') {
       $('#content_discussion_form_subject').attr('placeholder', 'Enter Youtube URL');
     }
 
-  if(type === 'Code')
-    {
+    if(type === 'Code') {
       $('#content_discussion_form_subject').attr('placeholder', 'Enter Gist URL');
     }
-};
+  });
+});
