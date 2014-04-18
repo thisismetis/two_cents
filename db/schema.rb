e3759b014add8cb013bdfd446c36a960cde16ef8
Sonia Espinosa <sespinosa@college.harvard.edu>
2014-04-18 18:22:54 -0400

    add video upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417211453) do
+ActiveRecord::Schema.define(version: 20140418220224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,5 +68,11 @@ ActiveRecord::Schema.define(version: 20140417211453) do
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  create_table "videos", force: true do |t|
+    t.string   "subject"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
