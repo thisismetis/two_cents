3491cf4b18fc0020807290b8acd98f073929f1f4
Justin Kenyon <kenyonj@gmail.com>
2014-04-19 01:37:57 -0400

    Add before_action to dashboards controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,6 @@
 class DashboardsController < ApplicationController
+  before_action :authorize
+
   def show
     @discussions = current_user.discussions
   end
--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,13 @@
 class WelcomesController < ApplicationController
+  before_action :goto_dashboard, if: :signed_in?
+
   def show
     @user = User.new
+  end
+
+  private
+
+  def goto_dashboard
+    redirect_to dashboard_path
   end
 end
